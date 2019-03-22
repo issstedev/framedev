25d5ae108f61e9ed0bfa6b81530e4ccbb1c12b69
Miguel Vazquez <mvazquez83@gmail.com>
2019-03-22 16:44:14 +0000

    avances 2 22-03-2019

---
+++ app/Models/Inventarios/Estadistica.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+use Helpme;
+use DB;
+
+class Estadistica extends Model
+{
+  protected $table = 'estadistica';
+  protected $primaryKey = 'id';
+  public $timestamps = false;
+
+
+}
