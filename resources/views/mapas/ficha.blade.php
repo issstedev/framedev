d778ab4e82d9306e3bfc20b46f85a7d513781802
Miguel Vazquez <mvazquez83@gmail.com>
2019-03-28 19:25:59 +0000

    Fix psr4 and init return button

--- resources/views/mapas/ficha.blade.php
+++ resources/views/mapas/ficha.blade.php
@@ -1,6 +1,8 @@
  @extends('plantilla.master')
 @section('content')
 
+<i class="fa fa-undo"> ::before </i>
+
  @include('mapas._ficha', ['establecimiento'=> $establecimiento ] ) 
 
  @include('mapas._fichaDetalle', ['establecimiento'=> $establecimiento ] ) 
